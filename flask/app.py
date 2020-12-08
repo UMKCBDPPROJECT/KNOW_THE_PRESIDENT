cda92b703c0f752808e377e21f50b93e6e7ef9c7
nithin <nithindukkipati@gmail.com>
2020-12-07 18:08:38 -0600

    WEB_UI

--- flask/app.py
+++ flask/app.py
@@ -5,9 +5,6 @@ import numpy as np
 import requests
 import json
 import threading
-
-
-
 # Define a flask app
 app = Flask(__name__, template_folder='Templates')
 
@@ -19,10 +16,9 @@ def process_eval():
 # @app.route('/', methods=['GET'])
 # def index():
 #    return render_template('fileUpload.html')
-
-@app.route('/Result', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def handle_form():
-        y=process_eval();
+        y=process_eval()
         Trump_Neutral = y['Trump_Neutral']
         Trump_Positive = y['Trump_Positive']
         Trump_Negative = y['Trump_Negative']
@@ -32,11 +28,9 @@ def handle_form():
         Total = y['No_One'] + Trump_Neutral+ Trump_Positive+ Trump_Negative+ Biden_Negative + Biden_Neutral+ Biden_Positive
         return render_template('fileUpload.html',Total = Total, Trump_Negative= Trump_Negative, Trump_Neutral = Trump_Neutral, Trump_Positive = Trump_Positive, Biden_Negative = Biden_Negative, Biden_Neutral= Biden_Neutral,
                                Biden_Positive = Biden_Positive)
-
-@app.route('/', methods=['GET', 'POST'])
-def handle_form1():
-        return render_template('FirstPage.html')
-
+# @app.route('/', methods=['GET', 'POST'])
+# def handle_form1():
+#         return render_template('FirstPage.html')
 if __name__ == "__main__":
     app.run()
 
